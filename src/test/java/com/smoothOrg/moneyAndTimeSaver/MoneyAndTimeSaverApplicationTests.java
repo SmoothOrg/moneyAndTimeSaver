8d2ace5c22917ccb4c5b9d13e1ded6141fb9ed0a
Akash Verma <akashvermaa0208@gmail.com>
2025-06-25 08:20:07 +0530

    Initialised backend

---
+++ src/test/java/com/smoothOrg/moneyAndTimeSaver/MoneyAndTimeSaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoothOrg.moneyAndTimeSaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyAndTimeSaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
